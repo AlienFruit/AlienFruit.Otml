8184c68bc7cbd592990619bfa9a645aa3c858011
Alexandr Prokopovich <alienfruit@ya.ru>
2024-08-30 17:08:18 +0200

    feat: add HostBuilder extension

---
+++ src/AlienFruit.Otml.Configuration/HostBuilderExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+
+namespace AlienFruit.Otml.Configuration
+{
+    public static class HostBuilderExtensions
+    {
+        public static IHostBuilder AddOtmlConfigurationFile(this IHostBuilder builder, string configurationFile)
+        {
+            return builder.ConfigureAppConfiguration((x, y) => y.AddConfiguration(CreateConfiguration(configurationFile)));
+        }
+
+        static IConfigurationRoot CreateConfiguration(string file)
+        {
+            var configBuilder = new ConfigurationBuilder();
+            configBuilder.Sources.Add(
+                new OtmlConfigurationSource
+                {
+                    OtmlFile = file
+                });
+            return configBuilder.Build();
+        }
+    }
+}
